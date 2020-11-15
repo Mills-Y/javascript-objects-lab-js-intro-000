459281a0d97a48d20a25ef431c70e795b1caa226
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-11-15 21:52:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ function updateObjectWithKeyAndValue (object, key, value) {
   object.key = "value"
   return object
 }
-
-function updateObjectWithKeyAndValue (object, key, value) {
-  objectName.keyName = "value"
-  return object
-}
