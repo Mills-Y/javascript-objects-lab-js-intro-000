8e325172e2256141d872bc4e4d917992e100e736
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-11-15 22:12:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function updateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 
-function updateObjectWithKeyAndValue(obj, key, value) { 
+function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
 }
