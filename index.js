993e4afce07908026c8693967d6e10dc74a41591
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-11-15 21:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var recipes = new Object ({
 })
 
 function updateObjectWithKeyAndValue (object, key, value) {
+  object.key = "value"
+  return object
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
   object.key = "value",
   object.key = "value"
   return object
